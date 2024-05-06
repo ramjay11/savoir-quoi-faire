c03ee82047c62c405c6b3d67722a2c1499fecaa8
Romorosa <romorosar@b2b.regn.net>
2024-05-06 11:02:21 +0800

    Engagement initial

---
+++ faire-du-back-end/src/main/java/com/ramjava/faire/du/back/end/FaireDuBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.faire.du.back.end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaireDuBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaireDuBackEndApplication.class, args);
+	}
+
+}
---
+++ faire-du-back-end/src/test/java/com/ramjava/faire/du/back/end/FaireDuBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.faire.du.back.end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaireDuBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
